0a0b2641b9f4554a9c8fb4712822ab6e2f0ef363
Pedro <antunespedro222@gmail.com>
2023-09-11 17:22:10 -0300

    fix: uso de dataFake para posts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { dataFake } from 'src/app/data/dataFake';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  posts: Array<any> = [''];
+  newPost: any = [''];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.loadPosts();
   }
 
+  loadPosts() {
+    this.newPost = dataFake[0];
+    this.posts = dataFake.slice(1);
+  }
 }
